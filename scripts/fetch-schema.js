69691655e72590b9a156052542c5810e1e899ada
kmax3ever <kmax3ever2@gmail.com>
2023-11-07 15:54:51 +0700

    fix get graphql schema

--- scripts/fetch-schema.js
+++ scripts/fetch-schema.js
@@ -9,20 +9,55 @@ const thegraphConfig = require('../graphql.thegraph.config')
 
 const exec = promisify(child_process.exec)
 
-function fetchSchema(url, outputFile) {
-  exec(`yarn --silent get-graphql-schema --h Origin=https://app.uniswap.org ${url}`)
+// function fetchSchema(url, outputFile) {
+//   exec(`yarn --silent get-graphql-schema --h Origin=https://app.uniswap.org ${url}`)
+//     .then(({ stderr, stdout }) => {
+//       if (stderr) {
+//         throw new Error(stderr)
+//       } else {
+//         fs.writeFile(outputFile, stdout)
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err)
+//       console.error(`Failed to fetch schema from ${url}`)
+//     })
+// }
+
+
+function fetchSchemaData() {
+  exec(`yarn get-graphql-schema --h Origin=https://app.uniswap.org https://api.uniswap.org/v1/graphql`)
     .then(({ stderr, stdout }) => {
+      fs.writeFile(`./src/graphql/data/schema.graphql`, stdout)
       if (stderr) {
         throw new Error(stderr)
       } else {
-        fs.writeFile(outputFile, stdout)
+       
       }
     })
     .catch((err) => {
       console.error(err)
-      console.error(`Failed to fetch schema from ${url}`)
+      console.error(`Failed to fetch schema fromxx`)
     })
 }
 
-fetchSchema(process.env.THE_GRAPH_SCHEMA_ENDPOINT, thegraphConfig.schema)
-fetchSchema(process.env.REACT_APP_AWS_API_ENDPOINT, dataConfig.schema)
+function fetchSchemaTheGraph() {
+  exec(`yarn get-graphql-schema --h Origin=https://app.uniswap.org https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3`)
+    .then(({ stderr, stdout }) => {
+      fs.writeFile(`./src/graphql/thegraph/schema.graphql`, stdout)
+      if (stderr) {
+        throw new Error(stderr)
+      } else {
+       
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      console.error(`Failed to fetch schema fromxx`)
+    })
+}
+fetchSchemaTheGraph()
+fetchSchemaData()
+// fetchSchema(process.env.THE_GRAPH_SCHEMA_ENDPOINT, thegraphConfig.schema)
+// fetchSchema(process.env.REACT_APP_AWS_API_ENDPOINT, dataConfig.schema)
+  
